18353d8db5fc372f6fb0f23547bc7d786c184be6
Peter Aspiazu <paspiazusabando@gmail.com>
2024-06-21 19:27:24 -0500

    agregando cambios nuevos otra vez

--- components/homePage/experience/SectionServiceContainer.tsx
+++ components/homePage/experience/SectionServiceContainer.tsx
@@ -7,21 +7,22 @@ export const SectionServiceContainer: FC = () => {
             <div className="sectionservicecontainer__container">
                 <ServiceComponent 
                     title="Roofing"
-                    description="Protect your most significant investment with a beautiful, long-lasting roof installed by us! Click for more information."
+                    description="Let’s protect and enhance your home with our expert roofing solutions!"
                     image="/images/web_shutterstock_676232578.jpg"
                     link="/service/roofing"
                     />
                 <ServiceComponent 
                     title="Siding"
-                    description="From traditional horizontal siding to faux wood shakes and more, we back all our siding work. Click for more details."
+                    description="Let’s completely protect your home from the elements!"
                     image="/images/web_shutterstock_700859902.jpg"
                     link="/service/siding"
                     />
                 <ServiceComponent 
-                    title="Decks"
-                    description="Add some outdoor living space to your home today with a brand-new deck. Click for more information & free estimate."
-                    image="/images/web_shutterstock_374922070.jpg"
-                    link="/service/decks"
+                    title="Drywall"
+                    description="Let’s strengthen and secure your structures with our expert welding solutions!
+"
+                    image="/images/drywall.jpg"
+                    link="/service/drywall"
                 />
             </div>
         </div>
--- data/searchData.ts
+++ data/searchData.ts
@@ -4,69 +4,69 @@ export const searchData = [
         title:"Do you provide free estimates?",
         description:"Yes. We provide free written estimates."
     },
-    {
-        id:2,
-        title:"How long has Besroi been in business?",
-        description:"Besroi was established in 1960. We have been in business for over 50 years."
-    },
-    {
-        id:3,
-        title:"What are the qualifications of your salesmen?",
-        description:"All our salesmen are former installers with 20 or more years of experience."
-    },
-    {
-        id:4,
-        title:"How do I know if I need a new roof?",
-        description:"Curling, cupping or blistering of the shingles; dark patches on the roof where the roofing granules have worn away- these are signs that your roof is nearing the end of its life. If your roof is approaching the 15-year mark, call our office and one of our knowledgeable salesmen will examine and evaluate your roof."
-    },
+    // {
+    //     id:2,
+    //     title:"How long has Besroi been in business?",
+    //     description:"Besroi was established in 1960. We have been in business for over 50 years."
+    // },
+    // {
+    //     id:3,
+    //     title:"What are the qualifications of your salesmen?",
+    //     description:"All our salesmen are former installers with 20 or more years of experience."
+    // },
+    // {
+    //     id:4,
+    //     title:"How do I know if I need a new roof?",
+    //     description:"Curling, cupping or blistering of the shingles; dark patches on the roof where the roofing granules have worn away- these are signs that your roof is nearing the end of its life. If your roof is approaching the 15-year mark, call our office and one of our knowledgeable salesmen will examine and evaluate your roof."
+    // },
     {
         id:5,
         title:"Will you put a dumpster at my house?",
         description:"We provide our crews with smaller dump trailers to lessen the weight in the driveway."
     },
-    {
-        id:6,
-        title:"Do I need ice protection with my new roof?",
-        description:"Yes. Per New York State code."
-    },
-    {
-        id:7,
-        title:"I am building a new home. Do you provide any new construction services?",
-        description:"Yes. We install roofing, siding, and gutters for many new home builders in WNY."
-    },
-    {
-        id:8,
-        title:"What type of contract do you use and are you insured?",
-        description:"We use an attorney general approved contract that abides by NYS law 36-A. We meet all insurance requirements required by all townships."
-    },
-    {
-        id:9,
-        title:"Do I need a building permit?",
-        description:"Many towns in WNY require building permits. Besroi will obtain any permits needed."
-    },
-    {
-        id:10,
-        title:"Does your company do repair work?",
-        description:"Yes, we provide a full range of service work including roof repairs, roof tune-ups, and siding repairs."
-    },
-    {
-        id:11,
-        title:"Do you require a deposit?",
-        description:"Payment is due upon completion. A deposit is not required."
-    },
-    {
-        id:12,
-        title:"How long will my job take?",
-        description:"Every project is different depending on the nature of the work. Ask your salesman at the time of the estimate."
-    },
-    {
-        id:13,
-        title:"What is your warranty?",
-        description:"Besroi provides a 5-year labor warranty. Manufacturer’s warranties vary by product."
-    },
-    {
-        id:14,
-        title:"Are you a member of the Home Improvement Council, Better Business Bureau or Buffalo-Niagara Builders Association?",
-        description:"Yes. We are long-standing members of the HIC, BBB, and BNBA."
-    },
+    // {
+    //     id:6,
+    //     title:"Do I need ice protection with my new roof?",
+    //     description:"Yes. Per New York State code."
+    // },
+    // {
+    //     id:7,
+    //     title:"I am building a new home. Do you provide any new construction services?",
+    //     description:"Yes. We install roofing, siding, and gutters for many new home builders in WNY."
+    // },
+    // {
+    //     id:8,
+    //     title:"What type of contract do you use and are you insured?",
+    //     description:"We use an attorney general approved contract that abides by NYS law 36-A. We meet all insurance requirements required by all townships."
+    // },
+    // {
+    //     id:9,
+    //     title:"Do I need a building permit?",
+    //     description:"Many towns in WNY require building permits. Besroi will obtain any permits needed."
+    // },
+    // {
+    //     id:10,
+    //     title:"Does your company do repair work?",
+    //     description:"Yes, we provide a full range of service work including roof repairs, roof tune-ups, and siding repairs."
+    // },
+    // {
+    //     id:11,
+    //     title:"Do you require a deposit?",
+    //     description:"Payment is due upon completion. A deposit is not required."
+    // },
+    // {
+    //     id:12,
+    //     title:"How long will my job take?",
+    //     description:"Every project is different depending on the nature of the work. Ask your salesman at the time of the estimate."
+    // },
+    // {
+    //     id:13,
+    //     title:"What is your warranty?",
+    //     description:"Besroi provides a 5-year labor warranty. Manufacturer’s warranties vary by product."
+    // },
+    // {
+    //     id:14,
+    //     title:"Are you a member of the Home Improvement Council, Better Business Bureau or Buffalo-Niagara Builders Association?",
+    //     description:"Yes. We are long-standing members of the HIC, BBB, and BNBA."
+    // },
 ]
